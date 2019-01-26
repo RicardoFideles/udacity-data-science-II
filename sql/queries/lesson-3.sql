3a245cb51dfe8a5607a0eafbac4920f1d455da34
Ricardo <ricardo@ricardofideles.com.br>
2019-01-26 21:09:03 -0200

    Lesson 2 e 3

--- sql/queries/lesson-3.sql
+++ sql/queries/lesson-3.sql
@@ -50,3 +50,70 @@ FROM (SELECT total_amt_usd
       LIMIT 3457) AS Table1
 ORDER BY total_amt_usd DESC
 LIMIT 2;
+
+-- Qual account (conta), por nome, fez o primeiro pedido de todos? Sua solução deve ter o nome da conta e a data do pedido.
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY occurred_at
+LIMIT 1;
+
+-- Encontre o total de vendas em usd para cada conta. 
+-- Você deve incluir duas colunas - o total de vendas para os pedidos de cada empresa em usd e o nome da empresa.
+
+SELECT SUM(o.total_amt_usd), a.name
+FROM orders o
+JOIN accounts a ON o.account_id = a.id
+GROUP BY a.name
+ORDER BY a.name
+
+-- Por meio de qual channel (canal) ocorreu o web_event mais recente, 
+-- qual account (conta) estava associada a esse web_event? 
+-- Sua consulta deve retornar apenas três valores - date (data), channel (canal) e account name (nome da conta).
+
+SELECT w.occurred_at occurred_at, w.channel channel, a.name acc_name
+FROM web_events w
+JOIN accounts a ON w.account_id = a.id
+GROUP BY channel, acc_name
+ORDER BY occurred_at DESC
+LIMIT 1;
+
+-- Encontre o número total de vezes em que cada tipo de channel (canal) dos web_events foram usados. 
+-- Sua tabela final deve ter duas colunas - o channel (canal) e o número de vez que o canal foi usado.
+SELECT w.channel, COUNT(*)
+FROM web_events w
+GROUP BY w.channel      
+
+-- Quem foi o primary contact (contato inicial) associado ao primeiro web_event? 
+SELECT a.primary_poc primary_poc
+FROM web_events w
+JOIN accounts a ON w.account_id = a.id
+ORDER BY occurred_at 
+LIMIT 1 
+
+-- Qual foi o menor pedido feito por cada conta em termos de total usd (total em dólares)? 
+-- Dê apenas duas colunas - o name (nome) da conta e o total usd (total em dólares). 
+-- Ordene da menor quantia em dólares à maior.
+
+SELECT MIN(o.total_amt_usd) total_usd, a.name acc_name
+FROM orders o 
+JOIN accounts a ON o.account_id = a.id
+GROUP BY acc_name
+ORDER BY total_usd
+
+-- Encontre o número de sales reps (representantes de vendas) em cada região. 
+-- Sua tabela final deve ter duas colunas - a region (região) e o número de sales_reps (representantes de vendas). 
+-- Ordene do menor número de representantes ao maior.
+
+SELECT r.name, COUNT(*) num_reps
+FROM region r
+JOIN sales_reps s
+ON r.id = s.region_id
+GROUP BY r.name
+ORDER BY num_reps;
+
+
+
+
+
